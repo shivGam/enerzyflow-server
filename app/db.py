0f9ce23db9cec396dde47fdd06a3895010754c6b
shivGam <shivdattjangam2003@gmail.com>
2025-04-16 21:04:45 +0530

    Deploy c1

--- app/db.py
+++ app/db.py
@@ -5,10 +5,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def get_db_connection():
-    return psycopg2.connect(
-        dbname=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        host=os.getenv("DB_HOST", "localhost"),
-        port=os.getenv("DB_PORT", 5432)
-    )
+    database_url=os.getenv("DATABASE_URL")
+    return psycopg2.connect(database_url)
